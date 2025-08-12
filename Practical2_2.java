684b0b05620e52acb9cd9474bf4385ae75b2f19a
24ftt1826 <24FTT1826@student.pb.edu.bn>
2025-08-12 18:44:27 +0800

    Add files via upload

---
+++ Practical2_2.java
@@ -0,0 +1,9 @@
+public class Practical2_2 {
+
+    public static void main(String[] args) {
+         System.out.println("(\\__/) I am an");
+          System.out.println("( @w@)       OOO  W    W L");
+           System.out.println("/) W )      O   O  W W W L");
+            System.out.println(" ^  ^        OOO    W W  LLL");
+    }
+}
